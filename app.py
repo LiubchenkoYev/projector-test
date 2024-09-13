b239b6ea34f0c9a2abe84e95688f4af65873c4d0
LIUBCHENKO Yevheniia (YLIU_UA1) <yevheniia.liubchenko.part@servier.com>
2024-09-13 12:48:41 +0300

    simple calcsimple calculator:add and subtract two numbers

---
+++ app.py
@@ -0,0 +1,16 @@
+x = float(input('Enter first number: '))
+op = input('Enter operator (+,-): ')
+y = float(input('Enter second number: '))
+print(x, op, y)
+
+
+def calculate(x, y, op):
+    if op == '+':
+        result = x+y
+    elif op == '-':
+        result = x-y
+    return result
+
+
+result = calculate(x, y, op)
+print('=', result)
